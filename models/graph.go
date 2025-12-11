7ac54f4ec005a770237e1f7a690965400463a192
violinahs-codes <violina.h.sapan@gmail.com>
2025-12-11 17:52:42 +0700

    update

--- models/graph.go
+++ models/graph.go
@@ -1,14 +1,22 @@
 package models
 
 import (
-    "time"
-    "gorm.io/datatypes"
+	"time"
+
+	"gorm.io/datatypes"
 )
 
 type Graph struct {
-    GraphID        uint           `gorm:"primaryKey" json:"graph_id"`
-    ConsumptionID  uint           `gorm:"not null" json:"consumption_id"`
-    GraphType      string         `json:"graph_type"`
-    DataPoints     datatypes.JSON `json:"data_points"`
-    CreatedAt      time.Time      `json:"created_at"`
+	GraphID uint `gorm:"primaryKey" json:"graph_id"`
+	// Make this nullable for non-entry charts:
+	ConsumptionID *uint `json:"consumption_id,omitempty"`
+
+	// Add owner & range:
+	UserID    uint       `gorm:"not null;index" json:"user_id"`
+	GraphType string     `json:"graph_type"`
+	StartDate *time.Time `json:"start_date,omitempty"`
+	EndDate   *time.Time `json:"end_date,omitempty"`
+
+	DataPoints datatypes.JSON `json:"data_points"`
+	CreatedAt  time.Time      `json:"created_at"`
 }
